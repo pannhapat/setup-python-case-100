1e6838df715555dea2dba34c7af3f64f832faf11
pannhapat <pannhapat9@gmail.com>
2021-02-01 23:02:59 +0700

    Create case1.py

---
+++ case1.py
@@ -0,0 +1,18 @@
+## python
+sudo apt-get install python3-pip
+
+pip install --upgrade pip
+
+## virtualenv using pip3
+sudo pip3 install virtualenv 
+
+virtualenv venv 
+
+pip install requests
+
+pip install uvicorn[standard]
+
+pip install fastapi
+
+pip install numpy
+pip install pandas
